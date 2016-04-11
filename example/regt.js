642fbd17059fba0afa13014fa9977813406a25f4
Fritz Lin <uxfritz@163.com>
2016-04-11 13:51:04 +0800

    regt 邀请讨论/发消息/踢人/退群

--- example/regt.js
+++ example/regt.js
@@ -9,7 +9,7 @@ import Nixe from '../src/Nixe'
 const chance = new Chance()
 
 ;['imtest0400'].forEach(thread)
-// ['imtest0400', 'imtest0401', 'imtest0402'].forEach(thread)
+// ;['imtest0400', 'imtest0401', 'imtest0402'].forEach(thread)
 
 
 function thread(username) {
@@ -59,7 +59,7 @@ function thread(username) {
         const doc = global._wins.login.window.document
         doc.querySelector('[type=submit]').click()
       })
-      .wait(8000)
+      .wait(10000)
       .evaluate(() => !!global._wins.home)
     isHomeOpen.should.be.eql(true)
     console.log('login success:', username)
@@ -85,9 +85,41 @@ function thread(username) {
             input.dispatchEvent(ev)
           }, 2000)
         }, keyword)
-      console.log('search & set conver', keyword)
+      console.log('set conver', keyword)
 
-      const msg = `Hello Bitch! ${chance.sentence()}`
+      let msg = `Hello Bitch! ${chance.word()}`
+      await nixe.wait(5000)
+        .evaluate((msg) => {
+          const doc = global._wins.home.window.document
+          doc.querySelector('.favorite').click() // 确保添加联系人
+          const input = doc.querySelector('#editor')
+          input.innerHTML = msg
+          input.blur()
+          doc.querySelector('.send_btn').click()
+        }, msg)
+      console.log('send chat msg', msg)
+
+      // 测试环境开群 暂时不行 @肖逵
+      await nixe
+        .evaluate(() => {
+          const doc = global._wins.home.window.document
+          doc.querySelector('.add_group').click()
+        })
+        .wait(2000)
+        .evaluate(() => {
+          const doc = global._wins.mucform.window.document
+          // const items = doc.querySelectorAll('.creat_right li[data-custom-id*=imtest]')
+          const items = doc.querySelectorAll('.creat_right li')
+          ;[].forEach.call(items, function (item) { item.click() })
+        })
+        .wait(500)
+        .evaluate(() => {
+          const doc = global._wins.mucform.window.document
+          doc.querySelector('.blue_btn').click()
+        })
+      console.log('create group')
+
+      msg = `Hey All! ${chance.word()}`
       await nixe.wait(5000)
         .evaluate((msg) => {
           const doc = global._wins.home.window.document
@@ -96,7 +128,33 @@ function thread(username) {
           input.blur()
           doc.querySelector('.send_btn').click()
         }, msg)
-      console.log('input &  send msg', msg)
+      console.log('send group msg', msg)
+
+      await nixe.wait(10000)
+        .evaluate(() => {
+          const currConverId = global._store.getState().currConverId
+          const currUserId = global._store.getState().currUserId
+          const memIds = global._store.getState().allMems[currConverId]
+          memIds.forEach((userId) => {
+            if (userId === currUserId) return
+            global.sendKickPresence(currConverId, userId)
+          })
+        })
+      console.log('kick members')
+
+      await nixe.wait(2000)
+        .evaluate(() => {
+          const doc = global._wins.home.window.document
+          doc.querySelector('.op-btn-group .logout').click()
+        })
+        .wait(2000)
+        .evaluate(() => {
+          const md5 = window.require('md5')
+          const hash = md5('退出讨论组__退出讨论组后将无法再收发讨论组的消息，确定要退出吗？')
+          const doc = global._wins[`confirm/${hash}`].window.document
+          doc.querySelector('#closeBtn').click()
+        })
+      console.log('quit group')
     }
   }).catch((err) => {
     // throw err // will not work
